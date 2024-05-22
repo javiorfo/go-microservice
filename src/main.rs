1a2910c4023c9acbd0e0a15e99b96edb0f14eaed
Javier Orfo <javierorfo@prontonmail.com>
2024-05-22 15:33:51 -0300

    actix added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    let res = format!("Hello {}!", name);
+    HttpResponse::Ok().json(res)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
