4a564e1f4efa52dff4814447a08b846b73aae48d
Javier Orfo <javierorfo@prontonmail.com>
2024-06-21 13:18:27 -0300

    keycloak and fiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Nerzal/gocloak/v13"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+var (
+	client       *gocloak.GoCloak
+	realm        string
+	clientID     string
+	clientSecret string
+	keycloakURL  string
+)
+
+func init() {
+	client = gocloak.NewClient("http://localhost:8081")
+	realm = "orfosys"
+	clientID = "java-spring3-microservice"
+	clientSecret = "RqaTlO0d2OnBbeRuImNnbLWm5yZL66Mo"
+	keycloakURL = "http://localhost:8081"
 }
+
+func SecureEndpoint(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Authorization header missing"})
+	}
+
+	token := authHeader[len("Bearer "):]
+	rptResult, err := client.RetrospectToken(c.Context(), token, clientID, clientSecret, realm)
+	if err != nil || !*rptResult.Active {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid or expired token"})
+	}
+
+	return c.JSON(fiber.Map{"message": "You have accessed a protected endpoint!"})
+}
+
+func main() {
+	app := fiber.New()
+
+	// Use logger middleware
+	app.Use(logger.New())
+
+	// Define routes
+	app.Get("/orfosys/dummy", SecureEndpoint)
+
+	log.Fatal(app.Listen(":8080"))
+}
+
--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/javiorfo/go-microservice
 
 go 1.22.3
+
+require (
+	github.com/Nerzal/gocloak/v13 v13.9.0
+	github.com/gofiber/fiber/v2 v2.52.4
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
