586e26dae56bddbad7066f89b06f9a42a8cf3392
ChaoSystema <124217829+chaosystema@users.noreply.github.com>
2024-06-30 11:32:13 -0300

    Update routes.go

--- adapter/in/routes/routes.go
+++ adapter/in/routes/routes.go
@@ -2,12 +2,12 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/javiorfo/go-microservice/adapter/in/routes/handlers"
-	"github.com/javiorfo/go-microservice/application/in"
-	"github.com/javiorfo/go-microservice/domain/model"
+	"github.com/chaosystema/go-microservice/adapter/in/routes/handlers"
+	"github.com/chaosystema/go-microservice/application/in"
+	"github.com/chaosystema/go-microservice/domain/model"
 )
 
 func DummyRouter(app fiber.Router, service in.FindByIdUseCase[*model.Dummy]) {
-// 	app.Get("/orfosys/dummy", SecureEndpoint)
+// 	app.Get("/chaosystema/dummy", SecureEndpoint)
 	app.Get("/dummy", handlers.FindById(service))
 }
